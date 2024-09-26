df5c771618754d723d0af4d763564da6828bbf47
EnzoFabriciok <pauloeenzo33@gmail.com>
2024-09-26 12:48:23 -0300

    Primeiro Commit

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+var elementosDuvida = window.document.querySelectorAll('.duvida')
+
+elementosDuvida.forEach(function (duvida) {
+    duvida.addEventListener('click', function () {
+        duvida.classList.toggle('ativa')
+    })
+})
+
